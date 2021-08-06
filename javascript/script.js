19fb29b6afbbbaa5b6ff5818e6debba321dfb600
gheorghiciucgeorgia <georgia-elena.gheorghiciuc@studen.tuiasi.ro>
2021-08-06 15:41:45 +0300

    sign up modal

---
+++ javascript/script.js
@@ -0,0 +1,23 @@
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+//the toogle password icon
+//when you click on the eye to show you the password
+const tooglePassword=document.querySelector('#togglePassword');
+const password=document.querySelector('#password');
+//we took the elements in some variables
+
+//we must attach an event listener to the togglePassword icon and toggle the type attribut of the password field
+tooglePassword.addEventListener('click',function(e){
+    //toggle the type attribute
+    const type=password.getAttribute('type')=='password' ? 'text' : 'password';
+
+    password.setAttribute('type', type);
+});
