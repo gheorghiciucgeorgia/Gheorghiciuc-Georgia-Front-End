b8c43655e221a90bf24c3a37856c11c0ff9ebedf
gheorghiciucgeorgia <georgia-elena.gheorghiciuc@studen.tuiasi.ro>
2021-08-18 09:05:56 +0300

    cookies added

--- javascript/script.js
+++ javascript/script.js
@@ -74,5 +74,19 @@ $(function () {
         }
     });
 });
+//------------Cookie---------------
+const cookieContainer = document.querySelector(".cookie-container");
+const cookieButton = document.querySelector(".cookie-btn");
+
+cookieButton.addEventListener("click", () => {
+  cookieContainer.classList.remove("active");
+  localStorage.setItem("cookieBannerDisplayed", "true");
+});
+
+setTimeout(() => {
+  if (!localStorage.getItem("cookieBannerDisplayed")) {
+    cookieContainer.classList.add("active");
+  }
+}, 2000);
 
 
