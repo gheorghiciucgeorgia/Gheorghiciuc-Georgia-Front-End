3ed16e9d76cf22a20a94869f52ec00e31957bda9
gheorghiciucgeorgia <georgia-elena.gheorghiciuc@studen.tuiasi.ro>
2021-08-18 08:29:56 +0300

    changes in the homepage

--- javascript/script.js
+++ javascript/script.js
@@ -64,9 +64,9 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 //--------------------Load More button-----------//
-$(function loadCLP() {
-    $(".box-hidden").slice(0, 4).show();
-    $("#CLP-loadMore").on('click', function (e) {
+$(function () {
+    $(".box-hidden").slice(0, 2).show();
+    $("#loadMore").on('click', function (e) {
         e.preventDefault();
         $(".box-hidden:hidden").slice(0, 1).slideDown('fast','linear');
         if ($(".box-hidden:hidden").length == 0) {
