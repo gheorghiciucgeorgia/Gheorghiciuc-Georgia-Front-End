1df247c9ec2c1a65fa60dc6de707dca049c1e18b
gheorghiciucgeorgia <georgia-elena.gheorghiciuc@studen.tuiasi.ro>
2021-08-24 12:11:52 +0300

    PDP page media

--- javascript/PDP-script.js
+++ javascript/PDP-script.js
@@ -130,3 +130,32 @@ function openCity(evt, cityName) {
     evt.currentTarget.className += " active";
   }
   document.getElementById("defaultOpen").click();
+  //slide for media
+var slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+// Next/previous controls
+function plusSlides2(n) {
+  showSlides2(slideIndex2 += n);
+}
+
+// Thumbnail image controls
+function currentSlide2(n) {
+  showSlides2(slideIndex2 = n);
+}
+
+function showSlides2(n) {
+  var i;
+  var slides2 = document.getElementsByClassName("mySlides2");
+  var dots2 = document.getElementsByClassName("dot2");
+  if (n > slides2.length) {slideIndex2 = 1}
+  if (n < 1) {slideIndex2 = slides2.length}
+  for (i = 0; i < slides2.length; i++) {
+      slides2[i].style.display = "none";
+  }
+  for (i = 0; i < dots2.length; i++) {
+      dots2[i].className = dots2[i].className.replace(" active", "");
+  }
+  slides2[slideIndex2-1].style.display = "block";
+  dots2[slideIndex2-1].className += " active";
+}
